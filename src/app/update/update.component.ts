dc8e504194bc11f48131bb0245e8933ccd46f3f8
Fabiano Moreira <professor@fabianomoreira.com.br>
2023-08-24 10:36:02 -0300

    Crud_Completo

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-update',
@@ -15,15 +15,32 @@ export class UpdateComponent implements OnInit {
   /* Objeto que recebe a foto da API */
   imagem:any = {};
 
+   /* Criando o nosso tradutor */
+   httpOptions = {
+    headers: new HttpHeaders({'Content-Type' : 'application/json'})
+  };
+  
   /* ActivedRouter - permite pegar o id numa rota(url) */
-  constructor(private Activ: ActivatedRoute, private http: HttpClient){}
+  constructor(
+    private Activ: ActivatedRoute, 
+    private http: HttpClient,
+    private router: Router
+    ){}
 
   ngOnInit(): void {
-    /* Gaurda o id dentro da variavel routerId */
+    /* Guarda o id dentro da variavel routerId */
     this.routerId = this.Activ.snapshot.params['id'];
 
     if(this.routerId){
+      /* Pega a foto no banco de dados e passa para o objeto imagem */
       this.http.get('http://localhost:3000/fotos/' + this.routerId).subscribe(caixinha => this.imagem = caixinha);
     }
+  }
+
+  /* Método de edição chamado pelo ngSubmit */
+  editar(form:any){
+    this.http.put('http://localhost:3000/fotos/' + this.routerId, form.value, this.httpOptions).subscribe();
+
+    this.router.navigate(['/']);
   }
 }
